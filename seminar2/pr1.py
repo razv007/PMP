187b12fa8170c6b9b8eef09e83e98c3583dc901a
Rotaru Razvan <63795033+razv007@users.noreply.github.com>
2025-10-10 10:52:41 +0300

    Update pr1.py

--- seminar2/pr1.py
+++ seminar2/pr1.py
@@ -22,6 +22,15 @@ def theoretical_probability() -> float:
 N = 1000000
 est = simulate(1000000, 10)
 theo = theoretical_probability()
+print("probabilitate simulare:")
 print(est)
+print("probabilitate teoretica:")
 print(theo)
+print("diferenta de probabilitate")
 print(abs(est - theo))
+# probabilitate simulare:
+# 0.316633
+# probabilitate teoretica:
+# 0.31666666666666665
+# diferenta de probabilitate
+# 3.366666666665408e-05
